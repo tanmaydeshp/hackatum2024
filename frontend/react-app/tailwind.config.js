0419be521c0768d1672d95362df32c2eefa34164
Tanmay <ge94vem@tum.de>
2024-11-22 22:04:45 +0100

    tailwind config

--- frontend/react-app/tailwind.config.js
+++ frontend/react-app/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {fontFamily: {sans: ['Roboto', 'sans-serif']},
+    gridTemplateColumns: {'70/30' : '70% 28%', },
+  
+  },
   },
   plugins: [],
-}
-
+};
